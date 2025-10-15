c86d9c14f367a94ca9809a17ad3e32e960216e6a
Sean Callaway <seancallaway@gmail.com>
2025-10-15 14:43:25 -0700

    Implement GetAuctionById controller.

--- src/Auctions/controllers/auctions.go
+++ src/Auctions/controllers/auctions.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"errors"
+	"log/slog"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 
 	"github.com/seancallaway/CarBay/auction/dtos"
 	"github.com/seancallaway/CarBay/auction/inits"
@@ -12,7 +16,6 @@ import (
 
 func GetAuctions(ctx *gin.Context) {
 	var auctions []models.Auction
-	// inits.DB.Joins("Item").Preload("Item").Order("items.make ASC").Find(&auctions)
 	inits.DB.Joins("Item").
 		Preload("Item").
 		Order(`"Item"."make" ASC`).
@@ -21,4 +24,28 @@ func GetAuctions(ctx *gin.Context) {
 	auctionDTOs := dtos.ToAuctionDTOs(auctions)
 
 	ctx.JSON(http.StatusOK, gin.H{"data": auctionDTOs})
+}
+
+func GetAuctionById(ctx *gin.Context) {
+	rawId := ctx.Param("id")
+	slog.Info("==> " + rawId)
+	id, err := uuid.Parse(rawId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid UUID"})
+		return
+	}
+
+	var auction models.Auction
+	result := inits.DB.Preload("Item").First(&auction, id)
+	slog.Info("==>" + auction.ID.String())
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Not Found"})
+		} else {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request"})
+		}
+	} else {
+		auctionDTO := dtos.ToAuctionDTO(&auction)
+		ctx.JSON(http.StatusOK, gin.H{"data": auctionDTO})
+	}
 }
--- src/Auctions/main.go
+++ src/Auctions/main.go
@@ -26,6 +26,7 @@ func main() {
 	})
 
 	r.GET("/api/auctions", controllers.GetAuctions)
+	r.GET("/api/auctions/:id", controllers.GetAuctionById)
 
 	r.Run()
 }
